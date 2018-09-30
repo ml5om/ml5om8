5b89374532de39a011a75f7cc320834d3dca5273
ml5om <43390029+ml5om@users.noreply.github.com>
2018-09-30 10:57:11 +0300

    Update ml5om8.js

--- ml5om8.js
+++ ml5om8.js
@@ -27,7 +27,7 @@ client.on('message', function(message) {
 message.channel.send('Add a song name or song link :drum: ')
             return;
         }
-        if (queue.length > 0 || isPlaying) {
+        if (Queue.length > 0 || isPlaying) {
             getID(args, function(id) {
                 add_to_queue(id);
                 fetchVideoInfo(id, function(err, videoInfo) {
